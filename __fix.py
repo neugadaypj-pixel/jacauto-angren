b2e4f07a844edd05357ec5a3991cf0e41d89a5ac
Jamolbek Pulatov <neugadaypj@gmail.com>
2026-07-31 12:08:05 +0500

    Switch to official JAC | auto uzbekistan logo (photo_2026-02-03_11-41-51.png)

--- __fix.py
+++ __fix.py
@@ -1,14 +1,10 @@
 with open('d:/jacauto-angren/index.html', 'r', encoding='utf-8') as f:
     c = f.read()
 
-old = '<a href="#" class="logo"><span class="logo-jac">JAC</span><span class="logo-subtitle">MOTORS ANGREN</span></a>'
-new = '<a href="#" class="logo"><img src="https://jacauto.uz/wp-content/uploads/2024/08/Untitled-design-2.png" alt="JAC" class="logo-dark"><img src="https://jacauto.uz/wp-content/uploads/2024/08/Untitled-design-2.png" alt="JAC" class="logo-light"></a>'
-
-c = c.replace(old, new)
+# Replace the plain JAC logo with the JAC | auto uzbekistan logo
+c = c.replace('Untitled-design-2.png', 'photo_2026-02-03_11-41-51.png')
 
 with open('d:/jacauto-angren/index.html', 'w', encoding='utf-8') as f:
     f.write(c)
 
-print('OLD found:', old in c)
-print('logo-dark count:', c.count('logo-dark'))
-print('logo-light count:', c.count('logo-light'))
+print('Count:', c.count('photo_2026-02-03_11-41-51.png'))
