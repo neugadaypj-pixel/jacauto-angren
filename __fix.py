e026a6acb6e9f3ff52d74ffc5d8e8b1dc9979ad1
Jamolbek Pulatov <neugadaypj@gmail.com>
2026-07-31 12:18:42 +0500

    Use favicon JAC logo (car icon) as main site logo in header

--- __fix.py
+++ __fix.py
@@ -1,11 +1,12 @@
 with open('d:/jacauto-angren/index.html', 'r', encoding='utf-8') as f:
     c = f.read()
 
-favicon = '    <link rel="icon" href="https://jacauto.uz/wp-content/uploads/2026/01/cropped-Untitled_design__73_-removebg-preview-32x32.png" sizes="32x32">\n    <link rel="icon" href="https://jacauto.uz/wp-content/uploads/2026/01/cropped-Untitled_design__73_-removebg-preview-192x192.png" sizes="192x192">\n    <link rel="apple-touch-icon" href="https://jacauto.uz/wp-content/uploads/2026/01/cropped-Untitled_design__73_-removebg-preview-180x180.png">\n'
-
-c = c.replace('<link rel="stylesheet" href="css/style.css">', favicon + '    <link rel="stylesheet" href="css/style.css">')
+# Replace the header logo with the favicon JAC logo (the car icon)
+favicon_logo = 'cropped-Untitled_design__73_-removebg-preview-192x192.png'
+old_logo = 'Untitled-design-2.png'
+c = c.replace(old_logo, favicon_logo)
 
 with open('d:/jacauto-angren/index.html', 'w', encoding='utf-8') as f:
     f.write(c)
 
-print('Favicon added. Count:', c.count('apple-touch-icon'))
+print('Logo replaced with favicon. Count:', c.count(favicon_logo))
